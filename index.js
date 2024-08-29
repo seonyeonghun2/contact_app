a293a72c059a4df327ca11cd9403d7668349d53b
seonyeonghun2 <kjca1117@naver.com>
2024-08-29 18:04:49 +0900

    서버 미들웨어 연결

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 // ESM (ES6 Module, Ecma Script Module) : import, export
 // CommonJS Module : require, module.exports 또는 exports
-
 // const express = require('express')
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
-
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('GET 요청!')
-});
-app.post('/', function (req, res) {
-  res.send('POST 요청!')
-});
-app.put('/', function (req, res) {
-  res.send('PUT 요청!')
-});
-app.delete('/', function (req, res) {
-  res.send('DELETE 요청!')
-});
+import userRouter from "./routes/userRouter.js";
+
+// middleware
+app.use(express.json()); // json 데이터를 express에서 처리 가능
+app.use("/", userRouter);
 
 app.listen(3000)
