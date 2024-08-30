4ff7b58ba2aab7d90d6e89b1456b7e1a349092de
seonyeonghun2 <kjca1117@naver.com>
2024-08-30 18:05:25 +0900

    user 라우터 완료

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -10,7 +10,17 @@ const timeLog = (req, res, next) => {
 router.use(timeLog);
 
 // User(연락처) 생성
-router.post('/', userControl.createUser);
+router.get('/user/:id', userControl.findOneUser); // 동적파라미터
+router.post('/user', userControl.createUser);
+
+// Users(연락처) 조회
+router.get('/users', userControl.findAllUsers);
+
+router.delete('/users', userControl.removeAllUsers);
+router.delete('/user/:id', userControl.removeUser);
+
+// User 연락처 수정
+router.put('/user', userControl.updateUser);
 
 // module.exports = router; // CommonJS 내보내기 문법 --> 현재는, ESM 방식
 export default router;
