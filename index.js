53ecb1960a53caef79da2e538eb10dfefe14617e
seonyeonghun2 <kjca1117>
2024-09-04 17:47:46 +0900

    multer, static 미들웨어 추가 및 설정

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-// ESM (ES6 Module, Ecma Script Module) : import, export
-// CommonJS Module : require, module.exports 또는 exports
-// const express = require('express')
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 import dotenv from "dotenv";
 dotenv.config();
-import express from "express";
+import express, { urlencoded } from "express";
 const app = express();
+import nunjucks from "nunjucks";
+import morgan from "morgan";
+import multer from "multer";
 import contactRouter from "./routes/contactRouter.js";
+import noteRouter from "./routes/noteRouter.js";
 
 app.set("port", process.env.PORT || 3001);
-
+app.set("view engine", "html");
+nunjucks.configure("views", {
+  express: app,
+  watch: true,
+});
 // middleware
-app.use(express.json()); // json 데이터를 express에서 처리 가능
+app.use(morgan("dev"));
+app.use(express.json()); // json 데이터를 express에서 처리 가능]
+app.use(urlencoded({ extended: true }));
+app.use(multer().none());
+app.use("/static", express.static(path.join(__dirname, "public")));
+app.use("/", noteRouter);
 app.use("/api", contactRouter);
 
 // error middleware
@@ -23,7 +37,7 @@ app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
   res.status(err.status || 500);
-  // res.render('error');
+  res.render("error");
 });
 app.listen(app.get("port"), () => {
   console.log(`🚧 server start port : ${app.get("port")}`);
