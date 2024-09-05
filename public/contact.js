d2305488069ad23da347b2953a462adaa0ca8f4b
seonyeonghun2 <kjca1117>
2024-09-06 08:48:35 +0900

    연락처 삭제 - 클라이언트 구현

--- public/contact.js
+++ public/contact.js
@@ -7,13 +7,31 @@ window.addEventListener('DOMContentLoaded', () => {
       body: new FormData(contactForm),
     })
       .then((res) => {
-        console.log(res)
-        // if (res.status === 200) {
-        //   location.reload();
-        // }
+        // console.log(res)
+        if (res.status === 200) {
+          location.reload();
+        }
       })
       .catch((error) => {
         console.log('에러메세지 : ' + error)
       })
+  })
+  const deleteBtns = document.querySelectorAll('.delete_btn')
+  deleteBtns.forEach((btn) => {
+    btn.addEventListener('click', async (e) => {
+      const contactNum = e.target.dataset.contactNum
+      await fetch(`http://localhost:3001/api/contact/${contactNum}`, {
+        method: 'DELETE',
+      })
+        .then((res) => {
+          alert('삭제 성공!')
+          location.reload()
+        })
+        .catch((err) => {
+          alert('삭제 실패!')
+          console.log(err)
+        })
+      //location.reload()
+    })
   })
 })
